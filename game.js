9d752d8c58d651ccb16a447ee2db611991921c0f
Vitória Marques <vivi.ferrmarques@gmail.com>
2022-06-30 16:01:46 -0300

    Update game.js

--- game.js
+++ game.js
@@ -85,7 +85,7 @@ const createCard = (character) => {
   const front = createElement('div', 'face front');
   const back = createElement('div', 'face back');
 
-  front.style.backgroundImage = `url('../img/${character}.png')`;
+  front.style.backgroundImage = `url('https://vifmarques.github.io/Memory-Game/img/${character}.png')`;
 
   card.appendChild(front);
   card.appendChild(back);
